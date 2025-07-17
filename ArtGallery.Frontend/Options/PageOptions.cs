062044290a5e84ebc66b10a4ae6cf5d31ae7e624
Dirk Kok <the@dirkkok.nl>
2025-07-17 17:57:17 +0200

    opengraph properties for layout

--- ArtGallery.Frontend/Options/PageOptions.cs
+++ ArtGallery.Frontend/Options/PageOptions.cs
@@ -2,5 +2,7 @@ namespace ArtGallery.Frontend.Options;
 
 public class PageOptions {
 	public string Title { get; set; }
+	public string? OgDescription { get; set; }
+	public string? OgImageUrl { get; set; }
 	public bool Nsfw { get; set; }
 }
